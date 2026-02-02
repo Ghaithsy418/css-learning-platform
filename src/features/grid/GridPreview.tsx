67fe77bc5e2b8c15c48a0773212d817ad85d5222
Ghaithsy418 <ghaithsy418@gmail.com>
2026-02-02 03:22:19 +0300

    ltr for grid preview

--- src/features/grid/GridPreview.tsx
+++ src/features/grid/GridPreview.tsx
@@ -19,6 +19,7 @@ export const GridPreview: React.FC<GridPreviewProps> = ({
 }) => {
   return (
     <motion.div
+      dir="ltr"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
